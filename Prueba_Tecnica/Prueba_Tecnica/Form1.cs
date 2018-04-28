27434ec4e21a06a3fe396dcf2194c79f2c1654f3
Lautaro Rivedieu <lautarorivedieu@hotmail.com>
2018-04-28 07:08:33 -0700

    First commit

---
+++ Prueba_Tecnica/Prueba_Tecnica/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Net;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Web.Script.Serialization;
+using System.IO;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_Tecnica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Load();
+        }
+
+        private void Form1_Load()
+        {
+            listView1.View = View.Details;
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("Team Name", 100);
+            listView1.Columns.Add("Team Logo", 100);
+
+            string[] arr = new string[3];
+            ListViewItem itm;
+
+            WebRequest request = WebRequest.Create("https://randomuser.me/api?results100");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream DataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(DataStream);
+
+            string responseFromServer = reader.ReadToEnd();
+
+            //JavaScriptSerializer js = new JavaScriptSerializer();
+            //JSONPerson arra = js.Deserialize<JSONPerson>(responseFromServer);
+            DataContractJsonSerializer jsonObjectPersonInfo =
+            new DataContractJsonSerializer(typeof(JSONPerson));
+            MemoryStream stream1 =
+            new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+        
+            JSONPerson personInfoModel =
+                    (JSONPerson)jsonObjectPersonInfo.ReadObject(stream1);
+           
+
+
+            arr[0] = "Real Madrid";
+            arr[1] = "Puto";
+            itm = new ListViewItem(arr);
+            listView1.Items.Add(itm);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Prueba_Tecnica/Prueba_Tecnica/JSONPerson.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Prueba_Tecnica
+{
+    public class JSONPerson
+    {
+        [DataMember(Name = "results")]
+        public Person [] results { get; set; }
+        [DataMember(Name = "info")]
+        public Info info { get; set; }
+        
+    }
+
+    public class Person
+    {
+        public string gender { get; set; }
+        public Name name { get; set; }
+        public Location location { get; set; }
+        public string email { get; set; }
+        //public string login { get; set; }
+        public string dob { get; set; }
+        public string registered { get; set; }
+        public string phone { get; set; }
+        public string cell { get; set; }
+        //public string id { get; set; }
+        public Image picture { get; set; }
+    }
+
+    public class Info {
+        public string seed { get; set; }
+        public int results { get; set; }
+        public int page { get; set; } 
+        public string version { get; set; }
+    }
+
+
+    public class Name {
+        public string title { get; set; }
+        public string first { get; set; }
+        public string last { get; set; }
+    }
+
+    public class Location {
+        public string street { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int postcode { get; set; }
+    }
+    public class Image {
+        public string large { get; set; }
+        public string medium { get; set; }
+        public string thumbnail { get; set; }
+    }
+}
